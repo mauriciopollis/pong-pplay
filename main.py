0a396c7ca7f0a6053a6abdbc774f1256a476a070
Mauricio Pollis <mauriciopollis@gmail.com>
2023-05-06 04:53:30 -0300

    Adição das imagens dos pads e colocação deles na tela de jogo

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ import random
 #inicialização
 janela = Window(1280,720)
 bola = Sprite("assets/bola_terra_pequena.png",1)
+pad_direita = Sprite("assets/pad_imagem_reduzida.png")
+pad_esquerda = Sprite("assets/pad_imagem_reduzida.png")
 fundo = GameImage("assets/fundo_astronauta.jpg")
 bola.set_position(janela.width/2 - bola.width /2, janela.height/2 - bola.height/2)
-vel_x = random.randrange(1000)
-vel_y = random.randrange(1000)
+pad_direita.set_position(0, janela.height/2 - pad_direita.height/2)
+pad_esquerda.set_position(janela.width - pad_esquerda.width, janela.height/2 - pad_esquerda.height/2)
+vel_x = random.randrange(500)
+vel_y = random.randrange(500)
+vel_pad = 200
 
 #game loop
 while(True):
@@ -23,8 +28,14 @@ while(True):
         vel_x = -vel_x
     if(bola.y<0 or (bola.y+bola.height)>janela.height):
         vel_y = -vel_y
+    
+    #implementar o movimento dos pads
+
+    #implementar colisão dos pads com a bola
         
     #desenho
     fundo.draw()
+    pad_direita.draw()
+    pad_esquerda.draw()
     bola.draw()
     janela.update()
