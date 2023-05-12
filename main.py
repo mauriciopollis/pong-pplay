dd3abd52618fbf711e7e3ddb2cf3dba176c5e32f
Mauricio Pollis <mauriciopollis@gmail.com>
2023-05-12 12:33:44 -0300

    Resolve bug da patinação

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ janela = Window(1280,720)
 bola = Sprite("assets/bola_terra_pequena.png",1)
 fundo = GameImage("assets/fundo_astronauta.jpg")
 bola.set_position(janela.width/2 - bola.width /2, janela.height/2 - bola.height/2)
-vel_x = random.randrange(500)
-vel_y = random.randrange(500)
+vel_x = random.randrange(1000)
+vel_y = random.randrange(1000)
 
 #game loop
 while(True):
@@ -19,10 +19,23 @@ while(True):
     dt = janela.delta_time()
     bola.x += vel_x * dt
     bola.y += vel_y * dt
-    if(bola.x<0 or (bola.x+bola.width)>janela.width):
+
+    if(bola.x<0):
+        bola.x = 0
         vel_x = -vel_x
-    if(bola.y<0 or (bola.y+bola.height)>janela.height):
+    if((bola.x+bola.width)>janela.width):
+        bola.x = janela.width - bola.width
+        vel_x = -vel_x
+    if(bola.y<0):
+        bola.y = 0
         vel_y = -vel_y
+    if((bola.y+bola.width)>janela.height):
+        bola.y = janela.height - bola.width
+        vel_y = -vel_y
+    #if(bola.x<0 or (bola.x+bola.width)>janela.width):
+    #    vel_x = -vel_x
+    #if(bola.y<0 or (bola.y+bola.height)>janela.height):
+    #    vel_y = -vel_y
           
     #desenho
     fundo.draw()
